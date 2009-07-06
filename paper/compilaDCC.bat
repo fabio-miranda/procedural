96b849461fc8a57183a82cfffeaad81c11dab6d6
fabiom <fabiom@01b71de8-32d4-11de-96ab-f16d9912eac9>
2009-07-06 16:37:11 +0000

    paper

---
+++ paper/compilaDCC.bat
@@ -0,0 +1,6 @@
+cd C:\Temp\MiKTeX 2.7\miktex\bin
+c:
+pdflatex C:\Temp\fabio\paper\paper.tex --output-directory C:\Temp\fabio\paper
+pdflatex C:\Temp\fabio\paper\paper.tex --output-directory C:\Temp\fabio\paper
+pdflatex C:\Temp\fabio\paper\paper.tex --output-directory C:\Temp\fabio\paper
+@pause
